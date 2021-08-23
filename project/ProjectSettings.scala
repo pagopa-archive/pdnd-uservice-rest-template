7a57497319e856d82bac2a4a079a77323d7b8f0e
David Greco <greco@acm.org>
2021-08-23 14:39:24 +0200

    modified the logic to compute the version

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -1,47 +1,31 @@
-import sbt.{Project, ThisBuild}
+import sbt.Project
 import sbtbuildinfo.BuildInfoKeys.buildInfoOptions
 import sbtbuildinfo.BuildInfoPlugin.autoImport.{BuildInfoKey, buildInfoKeys}
 import sbtbuildinfo.{BuildInfoOption, BuildInfoPlugin}
-import sbtdynver.DynVerPlugin.autoImport.{dynverCurrentDate, dynverInstance, dynverSeparator, dynverSonatypeSnapshots}
-import sbtdynver.GitDescribeOutput
 
-import java.util.Date
-import scala.sys.process.Process
+import scala.sys.process._
 import scala.util.Try
 
 /** Allows customizations of build.sbt syntax.
   */
 object ProjectSettings {
 
-  private val describeOutput: Option[GitDescribeOutput] = sbtdynver.DynVer.getGitDescribeOutput(new java.util.Date)
-
   //TODO since Git 2.22 we could use the following command instead: git branch --show-current
-  private val currentBranch: Option[String] = Try(
-    Process(s"git rev-parse --abbrev-ref HEAD").lineStream_!.head
-  ).toOption
+  private val currentBranch: Option[String] = Try(Process(s"git rev-parse --abbrev-ref HEAD").lineStream_!.head).toOption
+
   private val commitSha: Option[String] = Try(Process(s"git rev-parse --short HEAD").lineStream_!.head).toOption
 
-  private def getInterfaceVersion(date: Date) = {
-    val semver = raw"([1-9]\d*)\.(\d+)\.(\d+).*".r
-    sbtdynver.DynVer.version(date) match {
-      case semver(major, minor, _*) =>
-        s"$major.$minor"
-      case _ =>
-        "wrong-version"
-    }
+  private def getInterfaceVersion: String = {
+   val sv = ComputeVersion.version.split("\\.")
+   s"${sv(0)}.${sv(1)}"
   }
 
   //lifts some useful data in BuildInfo instance
-  val buildInfoExtra = Seq[BuildInfoKey](
-    "ciBuildNumber"       -> sys.env.get("BUILD_NUMBER"),
-    "commitSha"           -> commitSha,
-    "headDistanceFromTag" -> describeOutput.map(_.commitSuffix.distance),
-    "tag"                 -> describeOutput.map(_.ref.dropPrefix),
-    "currentBranch"       -> currentBranch,
-    BuildInfoKey.map(dynverInstance) { case (_, v) => "isDirty" -> v.isDirty },
-    BuildInfoKey.map(dynverInstance) { case (_, v) => "isSnapshot" -> v.isSnapshot },
-    BuildInfoKey.map(dynverInstance) { case (_, v) => "isVersionStable" -> v.isVersionStable },
-    BuildInfoKey.map(dynverCurrentDate) { case (_, v) => "interfaceVersion" -> getInterfaceVersion(v) }
+  val buildInfoExtra: Seq[BuildInfoKey] = Seq[BuildInfoKey](
+    "ciBuildNumber" -> sys.env.get("BUILD_NUMBER"),
+    "commitSha"             -> commitSha,
+    "currentBranch"         -> currentBranch,
+    "interfaceVersion"      -> getInterfaceVersion
   )
 
   /** Extention methods for sbt Project instances.
@@ -51,8 +35,6 @@ object ProjectSettings {
     def setupBuildInfo: Project = {
       project
         .enablePlugins(BuildInfoPlugin)
-        .settings(ThisBuild / dynverSonatypeSnapshots := true)
-        .settings(ThisBuild / dynverSeparator := "-")
         .settings(buildInfoKeys ++= buildInfoExtra)
         .settings(buildInfoOptions += BuildInfoOption.BuildTime)
         .settings(buildInfoOptions += BuildInfoOption.ToJson)
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("com.dwijnand" % "sbt-dynver" % "4.1.1")
+//addSbtPlugin("com.dwijnand" % "sbt-dynver" % "4.1.1")
 
 addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.10.0")
 
